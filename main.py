4bd8478da456f9a0ef84e555c1d837910a00b119
Jonathan Lopez <jonathanlop82@gmail.com>
2021-11-14 17:00:46 -0500

    Validation body

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ app = FastAPI()
 
 # Models
 
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -63,3 +68,21 @@ def show_person(
         description="This is a person id. It's requiered.")
         ):
     return {person_id: "It exist!"}
+
+
+# Validaciones: Request Body
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+    ...,
+    title="Person ID",
+    description="This is a person ID",
+    gt=0
+    ),
+    person: Person = Body(...),
+    Location: Location = Body(...)
+    ):
+        results = person.dict()
+        results.update(location.dict())
+        return results
